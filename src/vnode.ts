46ae1ee7366ed87c3769045d2ec74daa5b2f0549
yanglan <yanglan@yangqianguan.com>
2018-06-15 16:27:04 +0800

    [Cyber] build conf

---
+++ src/vnode.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: yanglan 
+ * @Date: 2018-06-15 14:25:05 
+ * @Last Modified by: yanglan
+ * @Last Modified time: 2018-06-15 16:20:45
+ */
+
+export function hello(name: string) {
+    return `hello ${name} !`;
+}
+
+export default hello
